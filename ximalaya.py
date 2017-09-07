5f991bbc9922a1a7b8c15d51ba7c61f9489b1dd1
colourful-tree-aliyun <colourful_tree@163.com>
2017-09-07 14:02:41 +0800

    ximalaya

--- ximalaya.py
+++ ximalaya.py
@@ -1,43 +1,63 @@
-# -*- coding:UTF-8 -*-
 import requests
-#import requests.packages
-#import requests.packages.urllib3.util.ssl_
 from bs4 import BeautifulSoup
+import json
+import urllib
 
-#requests.packages.urllib3.util.ssl_.DEFAULT_CIPHERS = 'ALL'
+def getHtml(url):
+    r = requests.get(url,headers={
+                                    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36'
+                })
+    return r.content
+aaa = []
+with open("url" ,"r") as fin:
+    for i in fin:
+        aaa.append(i.strip())
 
-def download_info(url):
-    try:
-        r = requests.get(url, headers = { 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.20 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/534.20' })
-    except requests.RequestException as e:
-        print e
-    html = r.content
-    print html
-    if html.find("北京") != -1 and html.find("关注她") != -1:
-        return True
-    else:
-        return False
+for aa in aaa:
+    url = "http://www.ximalaya.com/18522984/album/" + aa + "/"
+    print url
+    html = getHtml(url)
+#print html
 
-def main():
-    url = "https://www.zhihu.com/question/30400300"
-    page = 10
-    for i in range(1, page):
-        real_url = url + "/?page=" + str(i)
-        print real_url
-        try:
-            r = requests.get(real_url, headers = { 'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.20 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/534.20' })
-            html = r.content
-            print html
-            soup = BeautifulSoup(html, "html.parser")
-            content_list = soup.find_all("div", attrs={"class":"AuthorInfo"})
-            for i in content_list:
-                p = i.find("meta", attrs={"itemprop":"url"})
-                people_url = p["content"]
-                if download_info(people_url):
-                    print people_url
-        except requests.RequestException as e:
-            print e
+    soup = BeautifulSoup(html, "html.parser")
+    title = soup.find("div", attrs={"class":"detailContent_title"})
+    if title != None:
+        title = title.get_text().encode("utf-8").strip()
 
-        
+    sub = soup.find("div", attrs={"class":"mid_intro"})
+    if sub != None:
+        sub = sub.get_text().encode("utf-8").strip()
 
-main()
+    all_pages = []
+    next_page_list = soup.find("div", attrs={"class":"pagingBar_wrapper"})
+#print next_page_list
+    if next_page_list != None:
+        p_list = next_page_list.find_all("a")
+        #print len(p_list)
+        for i in p_list:
+            all_pages.append(("http://www.ximalaya.com" + i["href"]).encode("utf-8"))
+        all_pages = all_pages[1:-1]
+    all_pages = [url] + all_pages
+
+    pv = []
+    for each_page in all_pages:
+        html_i = getHtml(each_page)
+        soup_i = BeautifulSoup(html_i, "html.parser")
+        album_soundlist = soup_i.find("div", attrs={"class":"album_soundlist is_more"})
+        if album_soundlist == None:
+            album_soundlist = soup_i.find("div", attrs={"class":"album_soundlist"})
+
+        if album_soundlist != None:
+            p_list = album_soundlist.find_all("li")
+            for i in p_list:
+                k = i.find("a", attrs={"class":"title"}).get_text().strip().encode("utf-8")
+                v = i.find("span", attrs={"class":"sound_playcount"}).get_text().strip().encode("utf-8")
+                pv.append({k:v})
+    res = {}
+    res["title"] = title
+    res["abstract"] = sub
+    res["pv"] = pv
+    result = json.dumps(res, ensure_ascii=False)
+#print result
+    with open("res.txt", "a") as fout:
+        fout.write("\n" + result)
